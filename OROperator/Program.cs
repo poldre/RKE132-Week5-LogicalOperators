ab7b371e10f8bf01376476f040cb4e818a8ce3df
Tony. <antony.poldre@tktk.ee>
2024-04-13 20:10:52 +0300

    add week5 code

---
+++ OROperator/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your math result:");
+math = Int32.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter your chemistry result:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if(math >= 90 || chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your applications cannot be approved.");
+}
